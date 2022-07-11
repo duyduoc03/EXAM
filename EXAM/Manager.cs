8a0dd15af1d9d60647c6e9d0b7f8b886c016eea2
duyduoc03 <duyduoc03bg@gmail.com>
2022-07-11 11:17:42 +0700

    .

---
+++ EXAM/Manager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAM
+{
+    internal class Manager
+    {
+        private List<Product> Listsp = null;
+        Product sp = new Product();
+
+        public Manager()
+        {
+            Listsp = new List<Product>();
+        }
+
+        public int QuantitySP()
+        {
+            int Count = 0;
+            if (Listsp != null)
+            {
+                Count = Listsp.Count;
+            }
+            return Count;
+        }
+
+        public void EnterSP()
+        {
+            Console.Write("Enter ID: ");
+            sp.ID = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Enter Name: ");
+            sp.Name = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Enter Price ");
+            sp.Price = Convert.ToString(Console.ReadLine());
+
+            Listsp.Add(sp);
+
+        }
+        public void ShowSP(List<Product> listSP)
+        {
+            Console.WriteLine("{0, -5} {1, -20} {2, -5} ",
+                  "ID", "Name", "Price");
+            if (listSP != null && listSP.Count > 0)
+            {
+                foreach (Product x in listSP)
+                {
+                    Console.WriteLine("{0, -5} {1, -20} {2, -5} ",
+                                      x.ID, x.Name, x.Price);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void DeleteSP()
+        {
+            Console.WriteLine(" Delete Id");
+            sp.ID = Convert.ToString(Console.ReadLine());
+            Listsp.Remove(sp);
+        }
+        public List<Product> getListSP()
+        {
+            return Listsp;
+        }
+    }
+}
---
+++ EXAM/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\tMENU");
+                Console.WriteLine("1.Add productrecords.");
+                Console.WriteLine("2.Display productrecords.");
+                Console.WriteLine("3.Delete productby Id.");
+                Console.WriteLine("4.Exit.");
+                Console.WriteLine("Choose a number from 1 to 4");
+                int key = Convert.ToInt32(Console.ReadLine());
+                Manager mn = new Manager();
+                switch (key)
+                {
+                    case 1:
+                        Console.WriteLine("1. Add productrecords.");
+                        mn.EnterSP();
+                        Console.WriteLine("Add successful!");
+                        break;
+                    case 2:
+                        if (mn.QuantitySP() > 0)
+                        {
+                            Console.WriteLine("Display productrecords");
+                            mn.ShowSP(mn.getListSP());
+                        }
+                        else
+                        {
+                            Console.WriteLine("not product!");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Delete productby Id");
+                        mn.DeleteSP();
+                        Console.WriteLine("successful delete!");
+                        break;
+                    case 4:
+                        Console.WriteLine("Exit!");
+                        return;
+                    default:
+                        Console.WriteLine("Choose a number from 1 to 4");
+                        break;
+                }
+
+            }
+        }
+    }
+}
